6cdee92d328b8c310b8c87d0239ec3baf8d80193
Revatiyele <96041537+Revatiyele@users.noreply.github.com>
2022-03-21 14:29:36 +0530

    Update BootstrapDatePickerPage.java

--- src/test/java/ObjectRepository/BootstrapDatePickerPage.java
+++ src/test/java/ObjectRepository/BootstrapDatePickerPage.java
@@ -16,6 +16,8 @@ WebDriver driver;
 	}
 	
 	//BootStrap Date Picker locator
+	//BootStrap Date Picker locator
+
 
 
 	@FindBy(xpath = "//*[@class = 'dropdown-menu'] //*[text() = 'Bootstrap Date Picker']")
